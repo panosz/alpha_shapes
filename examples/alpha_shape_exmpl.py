9c9791fd18420027344192169e2b78ddd262b1c1
panosz <panosz@gmail.com>
2023-01-22 16:41:26 +0200

    alpha_shape_exmpl. no dependence on `descartes` package.

--- examples/alpha_shape_exmpl.py
+++ examples/alpha_shape_exmpl.py
@@ -1,19 +1,44 @@
-from descartes import PolygonPatch
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
 from alpha_shapes.alpha_shapes import Alpha_Shaper
 
 #  Define a set of points
 
-points = [(0.0, 2.1), (-0.25, 1.5), (0.25, 0.5), (-0.25, 1.25),
-          (0.75, 2.75), (0.75, 2.25), (0.0, 2.0), (1.0, 0.0),
-          (0.25, 0.15), (1.25, 1.5), (1.25, 1.25), (1.0, 2.1),
-          (0.65, 2.45), (0.25, 2.5), (0.0, 1.0), (0.5, 0.5),
-          (0.5, 0.25), (0.5, 0.75), (0, 1.25), (1.5, 1.5),
-          (1.0, 2.0), (0.25, 2.15), (1.0, 1.1), (0.75, 0.75),
-          (0.75, 0.25), (0.0, 0.0), (-0.5, 1.5), (1, 1.25),
-          (0.5, 2.5), (0.5, 2.25), (0.5, 2.75), (0.65, 0.45)]
-
+points = [
+    (0.0, 2.1),
+    (-0.25, 1.5),
+    (0.25, 0.5),
+    (-0.25, 1.25),
+    (0.75, 2.75),
+    (0.75, 2.25),
+    (0.0, 2.0),
+    (1.0, 0.0),
+    (0.25, 0.15),
+    (1.25, 1.5),
+    (1.25, 1.25),
+    (1.0, 2.1),
+    (0.65, 2.45),
+    (0.25, 2.5),
+    (0.0, 1.0),
+    (0.5, 0.5),
+    (0.5, 0.25),
+    (0.5, 0.75),
+    (0, 1.25),
+    (1.5, 1.5),
+    (1.0, 2.0),
+    (0.25, 2.15),
+    (1.0, 1.1),
+    (0.75, 0.75),
+    (0.75, 0.25),
+    (0.0, 0.0),
+    (-0.5, 1.5),
+    (1, 1.25),
+    (0.5, 2.5),
+    (0.5, 2.25),
+    (0.5, 2.75),
+    (0.65, 0.45),
+]
 
 
 points = list(set(points))
@@ -32,21 +57,28 @@ _, alpha_shape_unscaled = unnormalized_shaper.optimize()
 shaper = Alpha_Shaper(points, normalize=True)
 alpha_opt, alpha_shape_scaled = shaper.optimize()
 
-breakpoint()
 
 #  Compare the alpha shapes calculated with and without scaling.
-fig, (ax0, ax1, ax2) = plt.subplots(1, 3,
-                                    sharey=True,
-                                    sharex=True,
-                                    constrained_layout=True)
+fig, (ax0, ax1, ax2) = plt.subplots(
+    1, 3, sharey=True, sharex=True, constrained_layout=True
+)
 ax0.scatter(*zip(*points))
-ax0.set_title('data')
+ax0.set_title("data")
 ax1.scatter(*zip(*points))
 ax2.scatter(*zip(*points))
-ax1.add_patch(PolygonPatch(alpha_shape_scaled, alpha=0.2, color='r'))
+
+
+def plot_multipolygon(ax, multipolygon):
+    for p in multipolygon.geoms:
+        xe, ye = p.exterior.xy
+        ax.fill(xe, ye, alpha=0.2, fc="r", ec="b")
+
+
+plot_multipolygon(ax1, alpha_shape_scaled)
+
 ax1.set_title("with normalization")
-ax2.add_patch(PolygonPatch(alpha_shape_unscaled, alpha=0.2))
 ax2.set_title("without normalization")
+plot_multipolygon(ax2, alpha_shape_unscaled)
 
 for ax in (ax1, ax2):
     ax.set_axis_off()
