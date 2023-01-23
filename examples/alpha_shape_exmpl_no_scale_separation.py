a00d43737e475de43c1a4d003b18a112c54cfffb
panosz <panosz@gmail.com>
2023-01-23 11:40:45 +0200

    alpha_shape_exmpl_no_scale_separation. no dependence on `descartes` package.

--- examples/alpha_shape_exmpl_no_scale_separation.py
+++ examples/alpha_shape_exmpl_no_scale_separation.py
@@ -1,25 +1,49 @@
-from descartes import PolygonPatch
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
 from alpha_shapes.alpha_shapes import Alpha_Shaper
 
 #  Define a set of points
 
-points = [(0.,    0.),    (0.,    1.),    (1.,    1.1),
-          (1.,    0.),    (0.25,  0.15),  (0.65,  0.45),
-          (0.75,  0.75),  (0.5,   0.5),   (0.5,   0.25),
-          (0.5,   0.75),  (0.25,  0.5),   (0.75,  0.25),
-          (0.,    2.),    (0.,    2.1),    (1.,    2.1),
-          (0.5,    2.5),
-          (-0.5,    1.5), (-0.25,    1.5),
-          (-0.25,    1.25), (0,    1.25),
-          (1.5,    1.5), (1.25,    1.5),
-          (1.25,    1.25), (1,    1.25),
-          (0.5,    2.25),
-          (1.,    2.),    (0.25,  2.15),  (0.65,  2.45),
-          (0.75,  2.75),  (0.5,   2.5),   (0.5,   2.25),
-          (0.5,   2.75),  (0.25,  2.5),   (0.75,  2.25)]
+points = [
+    (0.0, 0.0),
+    (0.0, 1.0),
+    (1.0, 1.1),
+    (1.0, 0.0),
+    (0.25, 0.15),
+    (0.65, 0.45),
+    (0.75, 0.75),
+    (0.5, 0.5),
+    (0.5, 0.25),
+    (0.5, 0.75),
+    (0.25, 0.5),
+    (0.75, 0.25),
+    (0.0, 2.0),
+    (0.0, 2.1),
+    (1.0, 2.1),
+    (0.5, 2.5),
+    (-0.5, 1.5),
+    (-0.25, 1.5),
+    (-0.25, 1.25),
+    (0, 1.25),
+    (1.5, 1.5),
+    (1.25, 1.5),
+    (1.25, 1.25),
+    (1, 1.25),
+    (0.5, 2.25),
+    (1.0, 2.0),
+    (0.25, 2.15),
+    (0.65, 2.45),
+    (0.75, 2.75),
+    (0.5, 2.5),
+    (0.5, 2.25),
+    (0.5, 2.75),
+    (0.25, 2.5),
+    (0.75, 2.25),
+]
 
+
+points = list(set(points))
 
 #  Calculate the optimal alpha shape
 shaper = Alpha_Shaper(points)
@@ -30,9 +54,17 @@ print(alpha_opt)
 #  Visualize
 fig, (ax0, ax1) = plt.subplots(1, 2)
 ax0.scatter(*zip(*points))
-ax0.set_title('data')
+ax0.set_title("data")
 ax1.scatter(*zip(*points))
-ax1.add_patch(PolygonPatch(shape, alpha=0.2, color='r'))
+
+
+def plot_multipolygon(ax, multipolygon):
+    for p in multipolygon.geoms:
+        xe, ye = p.exterior.xy
+        ax.fill(xe, ye, alpha=0.2, fc="r", ec="b")
+
+
+plot_multipolygon(ax1, shape)
 ax1.set_title("alpha shape")
 for ax in (ax0, ax1):
     ax.set_axis_off()
