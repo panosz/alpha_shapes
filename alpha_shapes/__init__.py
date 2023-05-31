510044645fb71f6c9d4ccff0900fbfcb6162419e
panosz <panosz@gmail.com>
2023-05-31 12:22:51 +0300

    cleanup. Explicitly re-export names

--- alpha_shapes/__init__.py
+++ alpha_shapes/__init__.py
@@ -1,3 +1,4 @@
 from .alpha_shapes import Alpha_Shaper
 from .plotting import plot_alpha_shape
 
+__all__ = ["Alpha_Shaper", "plot_alpha_shape"]
--- examples/alpha_shape_exmpl_no_scale_separation.py
+++ examples/alpha_shape_exmpl_no_scale_separation.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 from alpha_shapes import Alpha_Shaper, plot_alpha_shape
 
