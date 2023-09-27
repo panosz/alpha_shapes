1fbb92424da19a3faf87bf46af640f4c4c479964
panosz <panosz@gmail.com>
2023-09-27 22:13:25 +0300

    Cleanup `plotting`: Streamline Access to Exterior Coordinates in `_plot_polygon`
    - Switched from using the `xy` attribute to the `coords` attribute for
      accessing exterior coordinates.
    - This change eliminates the need for an additional `np.column_stack`
      operation, streamlining the code.
    - Aims to simplify the codebase and ensure uniformity in accessing
      coordinate attributes.

--- alpha_shapes/plotting.py
+++ alpha_shapes/plotting.py
@@ -1,6 +1,6 @@
 import numpy as np
-from matplotlib.path import Path
 from matplotlib.patches import PathPatch
+from matplotlib.path import Path
 
 
 def plot_alpha_shape(ax, alpha_shape):
@@ -18,8 +18,7 @@ def _plot_polygon(ax, polygon):
     Plot a polygon using matplotlib's PathPatch.
     see https://stackoverflow.com/a/70533052/6060982
     """
-    xe, ye = polygon.exterior.xy
-    exterior = Path(np.column_stack([xe, ye]))
+    exterior = Path(np.asarray(polygon.exterior.coords))
     holes = [Path(np.asarray(hole.coords)) for hole in polygon.interiors]
     path = Path.make_compound_path(exterior, *holes)
     patch = PathPatch(path, facecolor="r", lw=0.8, alpha=0.5, ec="b")
